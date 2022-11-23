82a0810d9b5624b507a27a40395abddc9493cf12
pablogwapo <107923995+pablogwapo@users.noreply.github.com>
2022-11-23 00:02:45 +0000

    Add files via upload

---
+++ Streamlit/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write("# Welcome to Deep-MED! 👋")
+st.markdown(
+        
+        """
+        Deep-Med is a revolutionary start-up that implements Machine Learning methods to solve real word problems in the medical field. 
+       
+        """
+    )
+ 
---
+++ Streamlit/pages/3_Contact us.py
@@ -0,0 +1,1 @@
+st.write('More stuff coming soon!')
